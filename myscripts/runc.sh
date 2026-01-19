4c07ad1394f1cd2a2e1b690bb0a58a72458bfed1
= <=>
2026-01-19 21:55:52 +0700

    update c running script

--- myscripts/runc.sh
+++ myscripts/runc.sh
@@ -6,7 +6,7 @@ file_name=$binary_file_name$file_type
 
 compile_type=$2
 
-if [$compile_type = "lm"];then 
+if [ "$compile_type" = "lm" ];then 
     gcc -o $binary_file_name $file_name -lm
     ./$binary_file_name
 else 
