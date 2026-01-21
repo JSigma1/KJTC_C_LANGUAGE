19e2da892f7ce14b5a32f2f7f18921ff086af93a
= <=>
2026-01-21 22:57:51 +0700

    change error code in 2 week homework

--- 2/2/ex9.c
+++ 2/2/ex9.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
-int main(){
-    char variable ;
-    printf("Enter a character: ");
-    scanf("%c",&variable);
-    switch (variable % 5)
-    {
-    case 0:
-        printf("Super Lucky \n");
-        break;
-    case 1:
-        printf("Lucky \n");
-        break;
-    case 2:
-        printf("Normal \n");
-        break;
-    case 3:
-        printf("Bad \n");
-        break;
-    case 4:
-        printf("Very Bad \n");
-        break;
-    default:
-        printf("Error \n");
-        break;
+int main() {
+    int A;
+    char c;
+
+    while (1) {
+        printf("Enter a number: ");
+
+        if (scanf("%d", &A) == 1) {
+            int res = A % 5;
+            if (res < 0) res += 5;
+
+            if (res == 0) printf("Super Lucky\n");
+            else if (res == 1) printf("Lucky\n");
+            else if (res == 2) printf("Normal\n");
+            else if (res == 3) printf("Bad\n");
+            else if (res == 4) printf("Very Bad\n");
+        } 
+        else {
+            scanf(" %c", &c); 
+
+            if (c == '*') {
+                break;
+            } else {
+                printf("error message\n");
+            }
+        }
     }
     return 0;
 }
